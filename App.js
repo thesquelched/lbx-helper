2f68a628634b12f761e532ccdf4d0ebf0a4462c3
Scott Kruger <scott@chojin.org>
2022-02-08 12:14:55 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,405 @@
+import 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import { Provider, connect, useDispatch, useSelector } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, Switch, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {
+ createDrawerNavigator,
+ DrawerContentScrollView,
+ DrawerItemList,
+ DrawerItem,
+} from '@react-navigation/drawer';
+import mech from './assets/mech.png';
+
+const Facing = {
+  L: 'Left',
+  F: 'Front',
+  B: 'Rear',
+  R: 'Right',
+};
+
+const Location = {
+  LT: 'Left Torso',
+  LL: 'Left Leg',
+  LA: 'Left Arm',
+  CT: 'Center Torso',
+  RT: 'Right Torso',
+  RL: 'Right Leg',
+  RA: 'Right Arm',
+  H: 'Head',
+  RLT: 'Rear Left Torso',
+  RCT: 'Rear Center Torso',
+  RRT: 'Rear Right Torso',
+};
+
+const clusterHitsTable = {
+  2:  [0, 0,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2],
+  3:  [0, 0,  1,  1,  1,  2,  3,  2,  2,  2,  3,  3,  3],
+  4:  [0, 0,  1,  2,  2,  2,  2,  3,  3,  3,  3,  4,  4],
+  5:  [0, 0,  1,  2,  2,  3,  3,  3,  3,  4,  4,  5,  5],
+  6:  [0, 0,  2,  2,  3,  3,  4,  4,  4,  5,  5,  6,  6],
+  7:  [0, 0,  2,  2,  3,  4,  4,  4,  4,  6,  6,  7,  7],
+  8:  [0, 0,  3,  3,  4,  4,  5,  5,  5,  6,  6,  8,  8],
+  9:  [0, 0,  3,  3,  4,  5,  5,  5,  5,  7,  7,  9,  9],
+  10: [0, 0,  3,  3,  4,  6,  6,  6,  6,  8,  8, 10, 10],
+  12: [0, 0,  4,  4,  5,  8,  8,  8,  8, 10, 10, 12, 12],
+  15: [0, 0,  5,  5,  6,  9,  9,  9,  9, 12, 12, 15, 15],
+  20: [0, 0,  6,  6,  9, 12, 12, 12, 12, 16, 16, 20, 20],
+  30: [0, 0, 10, 10, 12, 18, 18, 18, 18, 24, 24, 30, 30],
+  40: [0, 0, 12, 12, 18, 24, 24, 24, 24, 32, 32, 40, 40],
+};
+
+const hitLocationTable = {
+  [Facing.L]: [
+    null,
+    null,
+    Location.LT,
+    Location.LL,
+    Location.LA,
+    Location.LA,
+    Location.LL,
+    Location.LT,
+    Location.CT,
+    Location.RT,
+    Location.RA,
+    Location.RL,
+    Location.H,
+  ],
+  [Facing.F]: [
+    null,
+    null,
+    Location.CT,
+    Location.RA,
+    Location.RA,
+    Location.RL,
+    Location.RT,
+    Location.CT,
+    Location.LT,
+    Location.LL,
+    Location.LA,
+    Location.LA,
+    Location.H,
+  ],
+  [Facing.B]: [
+    null,
+    null,
+    Location.RCT,
+    Location.RA,
+    Location.RA,
+    Location.RL,
+    Location.RRT,
+    Location.RCT,
+    Location.RLT,
+    Location.LL,
+    Location.LA,
+    Location.LA,
+    Location.H,
+  ],
+  [Facing.R]: [
+    null,
+    null,
+    Location.RT,
+    Location.RL,
+    Location.RA,
+    Location.RA,
+    Location.RL,
+    Location.RT,
+    Location.CT,
+    Location.LT,
+    Location.LA,
+    Location.LL,
+    Location.H,
+  ]
+};
+
+function rollD6() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function roll2D6() {
+  let a = rollD6();
+  let b = rollD6();
+
+  return {
+    roll1: a,
+    roll2: b,
+    sum: a + b,
+  };
+}
+
+function Roll({roll}) {
+  return (
+    <View style={styles.row}>
+      <Text style={[styles.defaultText, {width: 38}]}>{roll.sum}</Text>
+      <Icon type='material-community' name={'dice-' + roll.roll1}/>
+      <Icon type='material-community' name={'dice-' + roll.roll2}/>
+    </View>
+  );
+}
+
+const initialSettings = {
+  floatingCrits: false,
+  confirmHeadHits: false,
+}
+
+function settingsReducer(state = initialSettings, action) {
+  switch (action.type) {
+    case 'settings/setFloatingCrits':
+      return {...state, floatingCrits: action.payload};
+    case 'settings/setConfirmHeadHits':
+      return {...state, confirmHeadHits: action.payload};
+    default:
+      return state;
+  }
+}
+
+let store = createStore(combineReducers({
+  settings: settingsReducer,
+}));
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem
+        label='Settings'
+        icon={(focused, color, size) => <Icon color={color} size={size} name='settings' />}
+        onPress={() => props.navigation.navigate('Settings')}/>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+function createAction(type, payload) {
+  return {
+    type: type,
+    payload: payload,
+  };
+}
+
+function SettingsScreen({ navigation }) {
+  const dispatch = useDispatch();
+  const settings = useSelector(state => state.settings);
+
+  return (
+    <View style={[styles.container, {
+      justifyContent: 'flex-start',
+    }]}>
+      <View style={styles.row}>
+        <Text style={styles.optionText}>Floating criticals</Text>
+        <Switch
+          value={settings.floatingCrits}
+          onValueChange={ (value) => dispatch(createAction('settings/setFloatingCrits', value)) } />
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.optionText}>Confirm head hits</Text>
+        <Switch
+          value={settings.confirmHeadHits}
+          onValueChange={ (value) => dispatch(createAction('settings/setConfirmHeadHits', value)) } />
+      </View>
+
+    </View>
+  );
+}
+
+function SizeSelector(sizes, stateValue, stateSetter) {
+  let buttons = sizes.map((size, idx) => {
+    return (
+      <TouchableOpacity
+        key={idx.toString()}
+        style={[
+          styles.sizeButton,
+          idx == 0 ? styles.buttonLeft : (idx == sizes.length - 1 ? styles.buttonRight : {}),
+          stateValue == size ? styles.focusedButton : {},
+         ]}
+        onPress={ () => stateSetter(size) } >
+        <Text style={styles.sideText}>{size}</Text>
+      </TouchableOpacity>
+    );
+  });
+
+  return (
+    <View style={[styles.row, styles.optionView]}>
+      <Text style={styles.optionText}>Size</Text>
+      <View style={[styles.container, {
+        flexDirection: 'row',
+      }]}>
+        {buttons}
+      </View>
+    </View>
+  );
+}
+
+function WeaponScreen({ navigation }) {
+  const settings = useSelector(state => state.settings);
+
+  const [size, setSize] = useState(20);
+  const [facing, setFacing] = useState(Facing.F);
+
+  const [rolls, setRolls] = useState({
+    clusterRoll: {sum: 0, roll1: 0, roll2: 0},
+    hits: 0,
+    rolls: [],
+  });
+
+  //useEffect(() => {
+  //  console.log('Size', size);
+  //  console.log('Roll state:', rolls);
+  //});
+  const facingView = (
+    <View style={styles.row}>
+      <Text style={styles.optionText}>Facing</Text>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={[
+            styles.facingButton,
+            styles.buttonLeft,
+            facing === Facing.L? styles.focusedButton : {},
+           ]}
+          onPress={ () => setFacing(Facing.L) } >
+          <Text style={styles.sideText}>{Facing.L}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.facingButton,
+            facing === Facing.F ? styles.focusedButton : {},
+          ]}
+          onPress={ () => setFacing(Facing.F) } >
+          <Text style={styles.sideText}>{Facing.F}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.facingButton,
+            facing === Facing.B ? styles.focusedButton : {},
+          ]}
+          onPress={ () => setFacing(Facing.B) } >
+          <Text style={styles.sideText}>{Facing.B}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.facingButton,
+            styles.buttonRight,
+            facing === Facing.R ? styles.focusedButton : {},
+          ]}
+          onPress={ () => setFacing(Facing.R) } >
+          <Text style={styles.sideText}>{Facing.R}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
+  let doRoll = () => {
+    let newClusterRoll = roll2D6();
+    let hits = clusterHitsTable[size][newClusterRoll.sum];
+    setRolls({
+      clusterRoll: newClusterRoll,
+      hits: hits,
+      rolls: [...Array(hits)].map(roll2D6),
+    });
+  };
+
+  const renderItem = ({item}) => (
+    <View style={styles.row}>
+      <Roll roll={item}/>
+      <Text style={styles.defaultText}>{hitLocationTable[facing][item.sum]}</Text>
+    </View>
+  );
+
+  let showResult = () => {
+    return (
+      <View>
+        <View style={styles.row}>
+          <Text style={styles.defaultText}>Cluster roll:</Text>
+          <Roll roll={rolls.clusterRoll}/>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.defaultText}>Cluster hits: {rolls.hits}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  //<Image key='1' source={mech} style={{ height: 400, width: 280 }}/>
+
+  const sizeView = SizeSelector([20, 10, 5, 2], size, setSize);
+
+  return (
+    <FlatList
+      ListHeaderComponent={
+      <>
+        <View style={styles.container}>
+          {sizeView}
+          {facingView}
+
+          <TouchableOpacity
+            onPress={doRoll}
+            style={styles.button}>
+            <Text style={styles.sideText}>
+              Roll!
+            </Text>
+          </TouchableOpacity>
+
+          {rolls.clusterRoll.sum > 0 && showResult()}
+        </View>
+      </>}
+      data={rolls.rolls}
+      keyExtractor={(roll, idx) => idx}
+      renderItem={renderItem}
+    />
+  );
+  //return (
+  //  <ScrollView style={styles.scrollContainer} contentContainerStyle={[styles.container, {flex: 0}]}>
+  //    {sizeView}
+
+  //    <TouchableOpacity
+  //      onPress={doRoll}
+  //      style={styles.button}>
+  //      <Text style={styles.sideText}>
+  //        Roll!
+  //      </Text>
+  //    </TouchableOpacity>
+
+  //    {rolls.clusterRoll.sum > 0 && showResult()}
+  //    <StatusBar style="auto" />
+  //  </ScrollView>
+  //);
+}
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+function MainStackScreen() {
+  return (
+    <Drawer.Navigator tabBarPosition='bottom' drawerContent={(props) => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name='LB-X Autocannon' component={WeaponScreen} />
+      <Drawer.Screen name='Long-Range Missiles (LRM)' component={WeaponScreen} />
+      <Drawer.Screen name='Medium-Range Missiles (MRM)' component={WeaponScreen} />
+      <Drawer.Screen name='Short-Range Missiles (SRM)' component={WeaponScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Group>
+            <Stack.Screen name='Weapons' component={MainStackScreen} />
+          </Stack.Group>
+          <Stack.Group screenOptions={{ presentation: 'modal' }}>
+            <Stack.Screen name='Settings' component={SettingsScreen} />
+          </Stack.Group>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
@@ -16,5 +409,62 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginHorizontal: '2%',
+  },
+  scrollContainer: {
+    backgroundColor: '#fff',
+  },
+  defaultText: {
+    fontSize: 20,
+    color: '#000',
+  },
+  sideText: {
+    fontSize: 20,
+    color: '#000',
+  },
+  optionText: {
+    fontSize: 20,
+    color: '#000',
+  },
+  button: {
+    backgroundColor: '#ccc',
+    padding: 12,
+    alignItems: 'center',
+  },
+  facingButton: {
+    backgroundColor: '#ccc',
+    marginHorizontal: 1,
+    padding: 12,
+  },
+  sizeButton: {
+    backgroundColor: '#ccc',
+    marginHorizontal: 1,
+    padding: 12,
+    width: 50,
+    alignItems: 'center',
+  },
+  buttonLeft: {
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  buttonRight: {
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  focusedButton: {
+    backgroundColor: '#aaa',
+  },
+  numberInput: {
+    height: 20,
+    marginHorizontal: '1%',
+  },
+  optionView: {
+    marginBottom: 20,
   },
 });
